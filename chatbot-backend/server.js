b584943dd245901046582247e91ceb9ed9c6d96c
Ramazan ERTAN <71939813+raerta@users.noreply.github.com>
2025-02-06 22:06:24 +0300

    Update server.js

--- chatbot-backend/server.js
+++ chatbot-backend/server.js
@@ -39,7 +39,7 @@ app.use((req, res, next)=> {
   
 
 
-mongoose.connect('mongodb://chatbotadmin:123456@185.255.94.141:27017/chatbot_db?authSource=chatbot_db&readPreference=primary&ssl=false').then(() => {
+mongoose.connect('mongodb://db.../chatbot_db?authSource=chatbot_db&readPreference=primary&ssl=false').then(() => {
   console.log('MongoDB connected');
 }).catch(err => console.log(err));
 
